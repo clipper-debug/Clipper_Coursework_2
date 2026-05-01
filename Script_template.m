6274a8bb61dec5734e6b30e0ccfd1699cbdcf5ac
clipper-debug <warmsk708aubreyoc@gmail.com>
2026-05-01 21:06:15 +0800

    DONE the preliminary task to make the LED blink

--- Script_template.m
+++ Script_template.m
@@ -1,11 +1,34 @@
-% Insert name here
-% Insert email address here
+% Ruixuan Zou
+% biyrz22@nottingham.edu.cn
 
+clear;
+clc;
 
 %% PRELIMINARY TASK - ARDUINO AND GIT INSTALLATION [5 MARKS]
 
 % Insert answers here
 
+% Establish Comm between arduino kit and matlab
+
+a = arduino("COM5", "Uno");
+
+% Turn LED ON (just as a copy line)
+% writeDigitalPin(a,'D8',1)
+
+% Turn LED OFF (SAME)
+% writeDigitalPin(a,'D8',0)
+
+% To make the LED blink at 0.5s interval 10 times
+
+for i = 1:10
+
+    writeDigitalPin(a,'D8',1);
+    pause(0.5);
+
+    writeDigitalPin(a,'D8',0);
+    pause(0.5);
+
+end    
 %% TASK 1 - READ TEMPERATURE DATA, PLOT, AND WRITE TO A LOG FILE [20 MARKS]
 
 % Insert answers here
